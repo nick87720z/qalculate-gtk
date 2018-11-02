84c38f0d8dab06197fb781d9ba2b3e536da61de4
Nikita Zlobin <nick87720z@gmail.com>
2018-11-03 00:18:10 +0500

    numpad linking another approach

--- src/interface.cc
+++ src/interface.cc
@@ -1429,14 +1429,16 @@ void create_main_window(void) {
 		* link_style_right  = "* { border-bottom-left-radius: 0; border-top-left-radius: 0; }";
 
 		#define add_style(w_str, style_str) do{                                                     \
-			p = gtk_css_provider_new();                                                              \
-			gtk_css_provider_load_from_data(p, style_str, -1, NULL);                                            \
-			c = gtk_widget_get_style_context(GTK_WIDGET(gtk_builder_get_object(main_builder, w_str))); \
+			p = gtk_css_provider_new();                                                                \
+			gtk_css_provider_load_from_data(p, style_str, -1, NULL);                                      \
+			c = gtk_widget_get_style_context(GTK_WIDGET(gtk_builder_get_object(main_builder, w_str)));       \
 			gtk_style_context_add_provider(c, GTK_STYLE_PROVIDER (p), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION); \
 		} while(0)
 
-		add_style("button_brace_open", link_style_top);
-		add_style("button_brace_close", link_style_bottom);
+		add_style("button_brace_wrap",  link_style_top);
+		add_style("button_brace_open",  link_style_mid);
+		add_style("button_brace_close", link_style_mid);
+		add_style("button_comma",       link_style_bottom);
 
 		add_style("button_seven", link_style_tl);
 		add_style("button_eight", link_style_mid);
@@ -1451,16 +1453,15 @@ void create_main_window(void) {
 		add_style("button_dot",   link_style_mid);
 		add_style("button_exp",   link_style_br);
 
-		add_style("button_del", link_style_left);
-		add_style("button_ac",  link_style_right);
+		add_style("button_divide", link_style_top);
+		add_style("button_times",  link_style_mid);
+		add_style("button_sub",    link_style_mid);
+		add_style("button_add",    link_style_bottom);
 
-		add_style("button_times",  link_style_tl);
-		add_style("button_divide", link_style_tr);
-		add_style("button_add", link_style_bl);
-		add_style("button_sub", link_style_br);
-
-		add_style("button_ans", link_style_left);
-		add_style("button_equals", link_style_right);
+		add_style("button_ac",     link_style_top);
+		add_style("button_del",    link_style_mid);
+		add_style("button_ans",    link_style_mid);
+		add_style("button_equals", link_style_bottom);
 
 		#undef get_c
 	}
